db3a6d3b1c131a0af68138f1864789c9ba2880c8
Milind Mishra <milind.mishra4@gmail.com>
2021-07-19 15:12:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import LandingContent from "./components/LandingContent";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <LandingContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
